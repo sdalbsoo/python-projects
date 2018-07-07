ee48daec144d8eb37068f8104df9760df88673d6
sdalbsoo <sdalbsoo@gmail.com>
2018-07-07 18:22:02 +0900

    Add broker and test-code

--- src/fruit.py
+++ src/fruit.py
@@ -12,7 +12,12 @@ class FruitSeller(object):
         return num_apple, change
 
     def __str__(self):
-        return "<FruitSeller: 남은 사과:{}, 얻은 돈:{}>".format(self.num_apples, self.money)  # noqa
+        return (
+            f"<FruitSeller: 남은 사과: {self.num_apples}, 얻은 돈:{self.money}>"
+        )
+
+    def __repr__(self):
+        return str(self)
 
 
 class FruitBuyer(object):
@@ -21,32 +26,27 @@ class FruitBuyer(object):
         self.num_apples = 0
 
     def buy_apple(self, seller, money):
-        num_apples, change = seller.sell_apple(money)
-        self.num_apples += num_apples     # 구매자가 얻는 사과 총 개수
-        self.money = self.money - money + change    # 구매자의 재산
+        num_apple, change = seller.sell_apple(money)
+        self.num_apples += num_apple  # 구매자가 얻는 사과 총 개수
+        self.money = self.money - money + change  # 구매자의 재산
         print(f"{money}천원어치 사과 주세요!")
 
     def __str__(self):
-        return "FruitBuyer : 받은 사과:{}, 남은 돈:{}>".format(self.num_apples, self.money)  # noqa
+        return (
+            f"FruitBuyer : 받은 사과:{self.num_apples}, "
+            f"남은 돈:{self.money}, 받은 거스름돈:{self.change}>"
+        )
+
+    def __repr__(self):
+        return str(self)
 
 
-seller1 = FruitSeller(30, 3000)  # seller1은 사과 30개 보유 및 3000원에 판매 중
-print(seller1)  # 사과 보유량 확인.
+class FruitBroker(object):
+    def __init__(self, seller_list):
+        self.cheap_seller = seller_list[0]
+        for seller in seller_list:
+            if self.cheap_seller.price_apple > seller.price_apple:
+                self.cheap_seller = seller
 
-buyer1 = FruitBuyer(50000)  # buyer1은 5만원 보유 중
-buyer1.buy_apple(seller1, 5000)  # 구매자가 5천원어치 사과를 주문
-print(buyer1, seller1)  # 구매자와 판매자의 상황 출력
-buyer1.buy_apple(seller1, 6000)  # 구매자가 6천원어치 사과를 주문
-print(buyer1, seller1)  # 구매자와 판매자의 상황 출력
-buyer1.buy_apple(seller1, 7000)  # 구매자가 7천원어치 사과를 주문
-print(buyer1, seller1)  # 구매자와 판매자의 상황 출력
-print('-------------------------------------------')
-print('-------------------------------------------')
-
-seller2 = FruitSeller(50, 2000)
-print(seller2)
-buyer2 = FruitBuyer(50000)
-buyer2.buy_apple(seller2, 7000)
-print(buyer2, seller2)
-buyer2.buy_apple(seller2, 9000)
-print(buyer2, seller2)
+    def sell_apple(self, money):
+        return self.cheap_seller.sell_apple(money)
--- src/test_fruit.py
+++ src/test_fruit.py
@@ -1,4 +1,29 @@
-from fruit import __init__, sell_apple, buy_apple
+from fruit import FruitBroker
+from fruit import FruitBuyer
+from fruit import FruitSeller
 
-def test_fruit():
 
+def test_fruit1():
+    seller = FruitSeller(30, 3000)
+    buyer = FruitBuyer(50000)
+    buyer.buy_apple(seller, 8000)
+    assert buyer.money == 44000
+    assert buyer.num_apples == 2
+    assert seller.num_apples == 28
+    assert seller.money == 6000
+
+
+def test_fruit2():
+    seller_list = [
+        FruitSeller(30, 3000), FruitSeller(50, 2000), FruitSeller(70, 1000)
+    ]
+    buyer = FruitBuyer(50000)
+    broker = FruitBroker(seller_list)
+    buyer.buy_apple(broker, 8000)
+    assert buyer.money == 42000
+    assert buyer.num_apples == 8
+    assert seller_list[0].num_apples == 30
+    assert seller_list[1].num_apples == 50
+    assert seller_list[2].num_apples == 62
+    assert seller_list[2].price_apple == 1000
+    assert seller_list[2].money == 8000
