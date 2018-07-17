79c661891bb73048ee540c0c7b858fceff366b4a
sdalbsoo <sdalbsoo@gmail.com>
2018-07-17 19:27:00 +0900

    [Sub-Project] To check DictParser plan

--- cheese/cheese.py
+++ cheese/cheese.py
@@ -1,10 +1,15 @@
 import re
 
+import requests
+
+import constants
+
 
 class SubtitleParser():
     def __init__(self):
-        with open("../data/srt/stopwords.txt", "r") as f:
-            self.stopwords = set(f.read().split('\n'))
+        self.meaning_words = DictParser()  # meaning_words가 DictParser를 가진다.
+        with open(constants.PATH_STOPWORDS, "r") as f:
+            self.stopwords = set(f.read().splitlines())
 
     def remove_tag(self, text, tag):
         regex = f"<{tag}>(.*?)</{tag}>"
@@ -18,7 +23,7 @@ class SrtParser(SubtitleParser):
     def __init__(self, srt_path):
         super(SrtParser, self).__init__()
         with open(srt_path, 'r') as f:
-            self.lines = f.read().splitlines()
+            self.lines = f.read().lower().splitlines()
 
     def extract_sentences(self):
         sentences = []
@@ -47,11 +52,22 @@ class SmiParser():
         pass
 
 
+class DictParser():
+    def searchdict(extractwords):
+        words = []
+        meaing_words = []
+        for key, value in extractwords.items():
+            words.append(key)
+        # requests모듈 이용해서 단어 검색하는 코드 추가한 뒤 뜻들을 meaning_words에 append
+        return meaning_words  # 영단어 뜻이 있는 리스트를 return
+
+
 def main():
     srt_path = "../data/srt/lionking.srt"
     srt = SrtParser(srt_path)
     sentences = srt.extract_sentences()
-    srt.extract_words(sentences)
+    extractwords = srt.extract_words(sentences)
+    meaning_words = DictParser.searchdict(extractwords)
 
 
 if __name__ == '__main__':
--- cheese/test_cheese.py
+++ cheese/test_cheese.py
@@ -1,32 +1,35 @@
 from cheese import SubtitleParser
 from cheese import SrtParser
+from cheese import DictParser
 
+def test_extract_meaning():
+    pass
 
 def test_extract_words():
     words = SrtParser("../data/srt/lionking.srt")
-    temps = words.extract_sentence()
-    out = words.extract_words(temps)
-    assert out == {'day': 1, 'arrive': 1,
-                   'planet': 1, 'blinking,': 1, 'step': 1, 'sun': 1, 'see': 1}
+    sentences = words.extract_sentences()
+    words= words.extract_words(sentences)
+    assert words == {'day': 1, 'arrive': 1,
+                     'planet': 1, 'blinking,': 1, 'see': 1, 'step': 1, 'sun': 1,}
 
 def test_extract_sentence():
     sentence = SrtParser("../data/srt/lionking.srt")
-    out = sentence.extract_sentence()
-    assert out == ["From the day we arrive",
-                   "On the planet",
-                   "And, blinking, step into the sun",
-                   "There's more to see",]  # noqa
+    sentences = sentence.extract_sentences()
+    assert sentences == ["from the day we arrive",
+                   "on the planet",
+                   "and, blinking, step into the sun",
+                   "there's more to see",]  # noqa
 
 def test_remove_tag():
     sub = SubtitleParser()
-    out = sub.remove_tag("<i>There's more to see</i>", "i")
-    assert out == "There's more to see"
+    sub_line = sub.remove_tag("<i>There's more to see</i>", "i")
+    assert sub_line == "There's more to see"
 
-    out = sub.remove_tag("<i>There's more to see</i>", "i")
-    assert out == "There's more to see"
+    sub_line = sub.remove_tag("<i>There's more to see</i>", "i")
+    assert sub_line == "There's more to see"
 
-    out = sub.remove_tag("<p>There's more to see</p>", "p")
-    assert out == "There's more to see"
+    sub_line = sub.remove_tag("<p>There's more to see</p>", "p")
+    assert sub_line == "There's more to see"
 
-    out = sub.remove_tag("<p>>There's more to see</p>", "p")
-    assert out == ">There's more to see"
+    sub_line = sub.remove_tag("<p>>There's more to see</p>", "p")
+    assert sub_line == ">There's more to see"
