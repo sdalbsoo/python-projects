90edb9b121f258772f2b737c40cb0e1b12974b21
sdalbsoo <sdalbsoo@gmail.com>
2018-07-24 10:29:54 +0900

    change num to nums

--- algorithm/test_ascending_order.py
+++ algorithm/test_ascending_order.py
@@ -2,26 +2,26 @@ import random
 
 
 def test_ascending():
-    num = [2, 1, 5, 3, 6, 2, 5]
-    for i in range(len(num)):
-        for j in range(len(num)-1):
-            if num[j] > num[j+1]:
-                temp = num[j]
-                num[j] = num[j+1]
-                num[j+1] = temp
+    nums = [2, 1, 5, 3, 6, 2, 5]
+    for i in range(len(nums)):
+        for j in range(len(nums)-1):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
     ans = [1, 2, 2, 3, 5, 5, 6]
-    assert num == ans
+    assert nums == ans
 
 
 def test_ascending_order():
     ans = random.sample(range(10), 5)
     stash = tuple(ans)
     ans.sort()
-    num = list(stash)
-    for i in range(len(num)):
-        for j in range(len(num)-1):
-            if num[j] > num[j+1]:
-                temp = num[j]
-                num[j] = num[j+1]
-                num[j+1] = temp
-    assert num == ans
+    nums = list(stash)
+    for i in range(len(nums)):
+        for j in range(len(nums)-1):
+            if nums[j] > nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+    assert nums == ans
