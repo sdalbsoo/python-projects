275c7c0ab489ee031f0260a1a4d1d10d0d3aa7e9
sdalbsoo <sdalbsoo@gmail.com>
2018-07-27 22:41:20 +0900

    [Algorithm] Add insert_sort

--- algorithm/sort.py
+++ algorithm/sort.py
@@ -12,10 +12,22 @@ def bubble_sort(nums):
     return nums_copy
 
 
+def insert_sort(nums):
+    for j in range(1, len(nums)):
+        key = nums[j]
+        index = j
+        while index > 0 and nums[index-1] > key:
+            nums[index] = nums[index-1]
+            index -= 1
+        nums[index] = key
+    return nums
+
+
 def main():
     nums = [9, 7, 1, 3, 6, 10, 1, 4]
     print(nums)
     print(bubble_sort(nums))
+    print(insert_sort(nums))
 
 
 if __name__ == "__main__":
--- algorithm/test_sort.py
+++ algorithm/test_sort.py
@@ -1,12 +1,14 @@
 import random
 
 from sort import bubble_sort
+from sort import insert_sort
 
 
 def test_sort_deterministic():
     nums = [2, 1, 5, 3, 6, 2, 5]
     ans = [1, 2, 2, 3, 5, 5, 6]
     assert bubble_sort(nums) == ans
+    assert insert_sort(nums) == ans
 
 
 def test_sort_stochastic():
@@ -14,3 +16,4 @@ def test_sort_stochastic():
         nums = random.sample(range(10), 5)
         sorted_ans = sorted(nums)
         assert bubble_sort(nums) == sorted_ans
+        assert insert_sort(nums) == sorted_ans
