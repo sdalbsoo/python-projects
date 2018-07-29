ad56f8921b1a61ccccdb3544500c2f6186ebb12d
sdalbsoo <sdalbsoo@gmail.com>
2018-07-29 15:59:08 +0900

    [Subtitle] modify variable, dictparser

--- cheese/cheese.py
+++ cheese/cheese.py
@@ -6,42 +6,54 @@ import requests
 
 import constants
 
+class NoMeaningWordException(Exception):
+    def __init__(self, value="NoMeaningWordException"):
+        self.value = value
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return self.value
+
 
 class SubtitleParser():
     def __init__(self):
-        self.meaning_words = DictParser()  # meaning_words가 DictParser를 가진다.
+        self.dict_parser = DictParser()
         with open(constants.PATH_STOPWORDS, "r") as f:
             self.stopwords = set(f.read().splitlines())
 
     def remove_tag(self, text):
-        cleaner = re.compile('<.*?>')
-        clean_text = re.sub(cleaner, '', text)
+        cleaner = re.compile("<.*?>")
+        clean_text = re.sub(cleaner, "", text)
         return clean_text
-        # regex = f"<{tag}>(.*?)</{tag}>"
-        # matches = re.finditer(regex, text, re.MULTILINE)
-        # match = list(matches)[0]
-        # assert len(match.groups()) == 1
-        # return match.groups()[0]
+
+    def extract_meanings(self, words):
+        return self.dict_parser.searchdict(words)
+
+    def extract_words(self, sentences):
+        words = {}
+        for sentence in sentences:
+            for word in sentence.split(" "):
+                if word not in self.stopwords:
+                    words[word] = words.get(word, 0) + 1
+        return words
 
     @abstractmethod
     def extract_sentences(self):
-        pass
-
-    @abstractmethod
-    def extract_word(self):
         pass
 
 
 class SrtParser(SubtitleParser):
     def __init__(self, srt_path):
         super(SrtParser, self).__init__()
-        with open(srt_path, 'r') as f:
+        with open(srt_path, "r") as f:
             replace_words = [
-                ',', '.', '!', '?'
+                ",", ".", "!", "?", '"',
             ]
             content = f.read()
             for replace_word in replace_words:
-                content.replace(replace_word, '')
+                content = content.replace(replace_word, "")
             self.lines = content.lower().splitlines()
 
     def extract_sentences(self):
@@ -49,21 +61,13 @@ class SrtParser(SubtitleParser):
         for line in self.lines:
             if line.isdigit():
                 continue
-            if '-->' in line:
+            if "-->" in line:
                 continue
             if len(line) == 0:
                 continue
             line = self.remove_tag(line)
             sentences.append(line)
         return sentences
-
-    def extract_words(self, sentences):
-        words = {}
-        for sentence in sentences:
-            for word in sentence.split(' '):
-                if word not in self.stopwords:
-                    words[word] = words.get(word, 0) + 1
-        return words
 
 
 class SmiParser(SubtitleParser):
@@ -73,46 +77,45 @@ class SmiParser(SubtitleParser):
     def extract_sentences(self):
         pass
 
-    def extract_words(self):
-        pass
-
 
 class DictParser():
-    def searchdict(extracted_words):
+    def __init__(self):
+        self.daum_url = "http://alldic.daum.net/search.do?q={}"
+
+    def searchdict(self, extracted_words):
         meaning_words = {}
         for i, word in enumerate(extracted_words):
-            daum_url = 'http://alldic.daum.net/search.do?q={}'
-            search_url = daum_url.format(word)
-            temp = []
-            response = requests.get(search_url)
-            source = response.text
-            soup = BeautifulSoup(source, 'lxml')
-            parsed = soup.find_all('span', class_='txt_search', limit=3)
-            for j in range(3):
-                try:
-                    if parsed[j].string is None:
-                        continue
-                    temp.append(parsed[j].string)
-                except IndexError:
-                    continue
-            meaning_words[word] = meaning_words.get(word, temp)
+            try:
+                url = self.daum_url.format(word)
+                source = requests.get(url).text
+                soup = BeautifulSoup(source, "lxml")
+                mdiv = soup.find(
+                    "div",
+                    attrs={"class": "cleanword_type kuek_type"}
+                )
+                if mdiv is None:
+                    raise NoMeaningWordException(f"NoMeaningWordException: No meaning for this word: {repr(word)}: {url}")  # noqa
+                meanings = [t.text for t in mdiv.find_all("li")]
+                meaning_words[word] = meanings
+                print(word, meanings)
+            except NoMeaningWordException as e:
+                print(e)
         return meaning_words
 
-    def remove_tag(text):
-        cleaner = re.compile('<.*?>')
-        clean_text = re.sub(cleaner, '', text)
+    def remove_tag(self, text):
+        cleaner = re.compile("<.*?>")
+        clean_text = re.sub(cleaner, "", text)
         return clean_text
 
 
 def main():
-    srt_path = "../data/srt/lionking.srt"
+    srt_path = "../data/srt/ironman.srt"
     srt = SrtParser(srt_path)
     sentences = srt.extract_sentences()
     extracted_words = srt.extract_words(sentences)
-    print(extracted_words)
-    meanings = DictParser.searchdict(extracted_words)
+    meanings = srt.dict_parser.searchdict(extracted_words)
     print(meanings)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- cheese/test_cheese.py
+++ cheese/test_cheese.py
@@ -4,28 +4,34 @@ from cheese import DictParser
 
 
 def test_srt_extract_meaning():
-    srt_file = SrtParser("../data/srt/lionking.srt")
-    sentences = srt_file.extract_sentences()
-    parser = srt_file.extract_words(sentences)
-    meanings = DictParser.searchdict(parser)
-    assert meanings == {'day': ['날'], 'arrive': ['도착하다'],
-                        'planet': ['행성'], 'blinking': ['가물거리는'],
-                        'see': ['보다'], 'step': ['단계'], 'sun': ['태양'],
-                        }
+    srt_parser = SrtParser("../data/srt/lionking.srt")
+    sentences = srt_parser.extract_sentences()
+    words = srt_parser.extract_words(sentences)
+    meanings = srt_parser.extract_meanings(words)
+    answers = {
+        "day": ['1.날', '2.하루', '3.낮', '4.데이', '5.시절'],
+        "arrive": ['1.도착하다', '2.오다', '3.가다', '4.도래하다', '5.도달하다'],
+        "planet": ['1.행성', '2.혹성', '3.유성'],
+        "blinking": ['1.깜박이는', '2.가물거리는', '3.지독한'],
+        "step": ['1.단계', '2.조치', '3.걸음', '4.계단', '5.스텝'],
+        "sun": ['1.태양', '2.해', '3.햇볕'],
+        "see": ['1.보다', '2.알다', '3.만나다', '4.발견하다', '5.이해하다'],
+    }
+    assert meanings == answers
 
 
 def test_srt_extract_words():
-    srt_file = SrtParser("../data/srt/lionking.srt")
-    sentences = srt_file.extract_sentences()
-    words = srt_file.extract_words(sentences)
-    assert words == {'day': 1, 'arrive': 1,
-                     'planet': 1, 'blinking': 1, 'see': 1, 'step': 1, 'sun': 1,
+    srt_parser = SrtParser("../data/srt/lionking.srt")
+    sentences = srt_parser.extract_sentences()
+    words = srt_parser.extract_words(sentences)
+    assert words == {"day": 1, "arrive": 1,
+                     "planet": 1, "blinking": 1, "see": 1, "step": 1, "sun": 1,
                      }
 
 
 def test_srt_extract_sentence():
-    srt_file = SrtParser("../data/srt/lionking.srt")
-    sentences = srt_file.extract_sentences()
+    srt_parser = SrtParser("../data/srt/lionking.srt")
+    sentences = srt_parser.extract_sentences()
     assert sentences == ["from the day we arrive",
                          "on the planet",
                          "and blinking step into the sun",
