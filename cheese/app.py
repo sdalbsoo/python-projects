9163aeeea897292ea3c9f40ae0f43f0dc0bcd358
sdalbsoo <sdalbsoo@gmail.com>
2018-07-29 17:12:31 +0900

    flask skeleton code

---
+++ cheese/app.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug import secure_filename
+
+from cheese import SrtParser
+
+UPLOAD_FOLDER = '/Users/Sdalbsoo/workspace/python-projects/cheese/files'
+ALLOWED_EXTENSIONS = set(['srt'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route("/")
+def index():
+    subtitle_path = request.args.get("path", None)
+    meanings = None
+    if subtitle_path is not None:
+        srt = SrtParser(subtitle_path)
+        sentences = srt.extract_sentences()
+        extracted_words = srt.extract_words(sentences)
+        word_meanings = srt.dict_parser.searchdict(extracted_words)
+    return render_template("index.html", path=subtitle_path, word_meanings=word_meanings)
+
+
+@app.route('/upload_file', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(path)
+            return redirect(url_for('index', path=path))
+        else:
+            return "Now allowed file"
+    else:
+        return "Something Wrong"
---
+++ cheese/readme.md
@@ -0,0 +1,8 @@
+# How to run
+
+When you are in development:
+
+```bash
+$ export FLASK_ENV=development
+$ flask run
+```
