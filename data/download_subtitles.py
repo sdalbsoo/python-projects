350499438001f883226be53558423e3f91e2cf2a
sdalbsoo <sdalbsoo@gmail.com>
2018-08-06 15:20:43 +0900

    [Selenium]Reflect review

--- data/download_subtitles.py
+++ data/download_subtitles.py
@@ -3,20 +3,33 @@ from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 
-list_urls = []
-for i in range(10):
-    resp = requests.get("https://english-subtitles.org/page/{}".format(i+1))
-    source = resp.text
-    soup = BeautifulSoup(source, "lxml")
-    parsed = [i for i in soup.find_all("div", attrs={"class": "tbl"})]
-    for j in parsed:
-        list_urls.append(j.find('a')['href'])
 
-driver = webdriver.Chrome("/Users/Sdalbsoo/Downloads/chromedriver")
-for i in list_urls:
-    driver.get(i)
-    driver.find_element_by_css_selector("input.downloadlink").click()
-    driver.find_element_by_css_selector("input.downloadlink").click()
-    driver.find_element_by_css_selector("a#logo").click()
-    time.sleep(5)
-driver.close()
+def get_url():
+    list_urls = []
+
+    for i in range(10):
+        resp = requests.get("https://english-subtitles.org/page/{}".format(i+1))  # noqa
+        source = resp.text
+        soup = BeautifulSoup(source, "lxml")
+        parsed = [i for i in soup.find_all("div", attrs={"class": "tbl"})]
+        for j in parsed:
+            list_urls.append(j.find('a')['href'])
+    return list_urls
+
+
+def main():
+    driver = webdriver.Chrome("/Users/Sdalbsoo/Downloads/chromedriver")
+    elements_css_selector = [
+        "input.downloadlink", "input.downloadlink", "a#logo",
+    ]
+
+    for url in get_url():
+        driver.get(url)
+        for i in elements_css_selector:
+            driver.find_element_by_css_selector(i).click()
+        time.sleep(5)
+    driver.close()
+
+
+if __name__ == "__main__":
+    main()
