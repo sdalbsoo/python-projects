39abaf9ee618267ff67a388fe549355b27e0ffa0
sdalbsoo <sdalbsoo@gmail.com>
2018-08-12 22:47:42 +0900

    [Selenium]Reflect review in progress

--- data/download_subtitles.py
+++ data/download_subtitles.py
@@ -1,42 +1,41 @@
 import time
-
 import argparse
+
 from bs4 import BeautifulSoup
 import requests
 from selenium import webdriver
 
 
-def get_url():
-    list_urls = []
-
+def get_urls():
     for i in range(10):
         resp = requests.get("https://english-subtitles.org/page/{}".format(i+1))  # noqa
         source = resp.text
         soup = BeautifulSoup(source, "lxml")
         parsed = [i for i in soup.find_all("div", attrs={"class": "tbl"})]
         for j in parsed:
-            list_urls.append(j.find('a')['href'])
-    return list_urls
+            yield j.find("a")["href"]
 
 
-def main():
+def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path_chromedriver", type=str, required=True, help="Need to get path of chromedriver")  # noqa
     parser.add_argument("-s", "--sleep", type=int, required=True, help="Need to get sleep time")  # noqa
     args = parser.parse_args()
+    return args
 
-    path = args.path_chromedriver
-    sleep_time = args.sleep
 
-    driver = webdriver.Chrome(path)
+def main():
+    args = argparser()
+    driver = webdriver.Chrome(args.path_chromedriver)
+
     elements_css_selector = [
         "input.downloadlink", "input.downloadlink", "a#logo",
     ]
-    for url in get_url():
+    for url in get_urls():
         driver.get(url)
         for i in elements_css_selector:
             driver.find_element_by_css_selector(i).click()
-        time.sleep(sleep_time)
+        time.sleep(args.sleep)
     driver.close()
 
 
