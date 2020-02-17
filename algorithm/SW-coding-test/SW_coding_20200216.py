78e9c69c4d6bc79c8c2e16acf50a3f26a8f05a02
SHIN BEOMSOO <sdalbsoo@gmail.com>
2020-02-17 12:09:02 +0900

    20200216 모의고사(완전탐색) 다른 사람의 풀이

--- algorithm/SW-coding-test/SW_coding_20200216.py
+++ algorithm/SW-coding-test/SW_coding_20200216.py
@@ -3,6 +3,7 @@
 공간복잡도: O(n^2)
 '''
 
+# 내 풀이
 def solution(answers):
     answer = []
     supoja = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
@@ -32,3 +33,29 @@ def logic(answers, length, supoja):
         if result[2][idx] == x:
             count3 += 1
     return [count1, count2, count3]
+
+# 다른 사람의 풀이
+'''
+시간복잡도: O(n)
+공간복잡도: O(n)
+'''
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
